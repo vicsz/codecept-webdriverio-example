f16deb950deb203b56b3eea9d43fc7248f1667aa
Victor Szoltysek <victor.szoltysek@flightnetwork.com>
2017-03-05 09:19:54 -0500

    Adding page example

---
+++ pages/home.js
@@ -0,0 +1,17 @@
+
+'use strict';
+
+let I;
+
+module.exports = {
+
+  _init() {
+    I = require('../steps_file.js')();
+  },
+
+  doSearch(query){
+  	I.say('Performing search for ' + query);
+  	I.fillField('search', query);
+  	I.click('[type=submit]');
+  }
+}
--- wikipedia_test.js
+++ wikipedia_test.js
@@ -1,8 +1,9 @@
 
 Feature('Wikipedia');
 
-Scenario('Wikipedia Home Page Test', (I) => {
+Scenario('Wikipedia Home Page Test', (I, homePage) => {
 	I.amOnPage('/');
   	I.seeInTitle('Wikipedia');
   	I.invokeCustomMethodFromI();
+  	homePage.doSearch('Toronto');
 });
